a0342e900c42fd6a7a31ea3e47f3739211396fc7
brj <brj@netcompany.com>
2024-01-19 08:44:30 +0100

    Initial commit

---
+++ components/LoginBox.server.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <!-- Hidden input field to hold the extracted token -->
+    <input type="hidden" v-model="extractedToken" />
+    
+    <!-- Display the extracted token (for demonstration) -->
+    <p>Extracted Token: {{ extractedToken }}</p>
+  </div>
+  
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const extractedToken = ref("")
+async function loadData(){
+    extractedToken.value = "value"
+  } 
+loadData()
+</script>
